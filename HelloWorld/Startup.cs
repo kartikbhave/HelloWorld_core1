c578015f0fd1dd4ccd164e2d8d4371339d6920d3
kartikbhave <kartikbhave.scs@gmail.com>
2018-03-27 18:09:55 +0530

    Update Startup.cs

--- HelloWorld/Startup.cs
+++ HelloWorld/Startup.cs
@@ -23,7 +23,7 @@ namespace HelloWorld
         }
 
         public IConfigurationRoot Configuration { get; }
-
+//this is test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
